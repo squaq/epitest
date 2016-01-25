7d53eae0696a541553fb5d40bf3935fe15b71158
Owner <owner@Owners-MacBook-Pro.local>
2016-01-25 12:03:00 -0300

    new project with tabs

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -40,46 +40,28 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.diario', {
+    url: '/diario',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-diario': {
+        templateUrl: 'templates/tab-diario.html',
+        controller: 'DiarioCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
 
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.mapa', {
+    url: '/mapa',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-mapa': {
+        templateUrl: 'templates/tab-mapa.html',
+        controller: 'MapaCtrl'
       }
     }
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/diario');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,153 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ["chart.js"])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DiarioCtrl', function($scope, $http, $ionicLoading) {
+    function httpGet(_url, success, error){
+        $http({ method: 'GET', url:_url}).then(success,error);
+    }
+    
+    $scope.init = function(){
+        $ionicLoading.show({
+          template: 'Carregando o Diário...'
+        });
+        
+        httpGet("../data/calendar_month.json", completeCM, erroCM);
+        httpGet("../data/calendar_year.json", completeCY, erroCY);
+        httpGet("../data/total.json", completeTotal, erroTotal);
+        
+        for(var i = 0; i < 3; i++){
+            var user = {}
+            user.id = "User "+(i+1)
+            user.name = "Nome"+(i+1)
+            $scope.users.push(user)
+        }
+        console.log($scope.users)
+    }
+    
+    $scope.users = []
+                
+    function completeCM(response)
+    {
+//        $scope.lineChart ={}
+//        $scope.lineChart.labels = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"]
+//        $scope.lineChart.data = []//[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+//        var mData = []
+//        $scope.lineChart.series = ['Mal']
+//        for(u in response.data.data){
+//            var pos = parseInt(response.data.data[u]._id.month)-1
+////            console.log(response.data.data[u])
+////                $scope.lineChart.data[pos] = response.data.data[u].count
+//            mData.push(response.data.data[u].count)
+////                $scope.lineChart.infos.push{{"month":$scope.lineChart.labels[u], "":}}
+//        }
+//        $scope.lineChart.data.push(mData)
+        hideLoad()
+    }
+    function erroCM(response)
+    {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    } 
+    
+    function completeCY(response)
+    {
+        $scope.lineChart ={}
+        $scope.lineChart.labels = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+        $scope.lineChart.data = []
+        var mData = [0,0,0,0,0,0,0,0,0,0,0,0]
+        $scope.lineChart.series = ['Mal']        
+        for (u in response.data.data)
+        {
+            mData[response.data.data[u]._id.month -1] = parseFloat(response.data.data[u].percent)
+        }
+        $scope.lineChart.data.push(mData)
+        hideLoad()
+    }
+    function erroCY(response)
+    {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    } 
+    
+    function completeTotal(response)
+    {
+        $scope.total = response.data.data;
+            $scope.pie={}
+            $scope.pie.data = [$scope.total.no_symptom, $scope.total.symptom]
+            $scope.pie.labels = ["Bem", "Mal"]
+            $scope.pie.colours = ["#1AFC0A", "#FC0A0A"]
+        hideLoad()
+    }
+    function erroTotal(response)
+    {
+
+    }
+    
+    $scope.goodPercent = function(){if($scope.total)return (($scope.total.no_symptom*100)/$scope.total.total).toFixed(2)}
+    $scope.badPercent = function(){if($scope.total)return (($scope.total.symptom*100)/$scope.total.total).toFixed(2)}
+    
+    
+    var contHideLd = 0
+    function hideLoad(){if(++contHideLd>=3)$ionicLoading.hide()}
+    
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+
+
+
+
+.controller('MapaCtrl', function($scope, $http, $compile, $ionicLoading) {
+    function httpGet(_url, success, error){
+        $http({ method: 'GET', url:_url}).then(success,error);
+    }
+    
+    $scope.initMap = function (){
+        $ionicLoading.show({
+          template: 'Carregando o Mapa...'
+        });
+        var myOptions = {
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        
+        var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+        $scope.map = map;
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude))
+        })
+        
+        google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
+            hideLoad();
+        });
+
+        
+        httpGet("../data/surveys.json", successCallback, errorCallback);
+    }
+    
+    var contHideLd = 0
+    function hideLoad(){if(++contHideLd>=2)$ionicLoading.hide()}
+    
+    function successCallback(response){
+       $scope.users = response.data.data;
+            $scope.$broadcast("scroll.refreshComplete")
+            for(i in response.data.data) {
+                
+                console.log(response.data.data[i]);
+                
+                var iconpath = "img/circle_red.png" 
+                if(response.data.data[i].no_symptom == "Y") var iconpath = "img/circle_green.png"
+                
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(response.data.data[i].lat, response.data.data[i].lon),
+                    map: $scope.map,
+                    icon: iconpath
+                });
+            }
+        hideLoad();
+    }
+    function errorCallback(response){
+       $scope.users = response.data.data;
+            $scope.$broadcast("scroll.refreshComplete")
+            $ionicLoading.hide();
+            console.log(response) 
+    }
 });
